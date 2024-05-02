eb2ea997c2c981859e8aeaadb42cf53262469253
Diarmuid Sheeran <dssheeran@outlook.com>
2024-05-02 20:52:21 +0000

    Add script logic to change the amount of calculators that appear depending on the number of players selected

--- script.js
+++ script.js
@@ -10,6 +10,12 @@ const playerThree = document.getElementById('playerThree');
 const playerFour = document.getElementById('playerFour');
 const teamOne = document.getElementById('teamOne');
 const teamTwo = document.getElementById('teamTwo');
+const playerOneCalc = document.getElementById('playerOneCalc');
+const playerTwoCalc = document.getElementById('playerTwoCalc');
+const playerThreeCalc = document.getElementById('playerThreeCalc');
+const playerFourCalc = document.getElementById('playerFourCalc');
+const teamOneCalc = document.getElementById('teamOneCalc');
+const teamTwoCalc = document.getElementById('teamTwoCalc');
 
 nextBtn.addEventListener('click', function() {
     startScreen.style.display = 'none';
@@ -42,4 +48,27 @@ nextBtn.addEventListener('click', function() {
 startBtn.addEventListener('click', function() {
     nameScreen.style.display = 'none';
     calculator.style.display = 'block';
-})
+    switch (numberOfPlayers.value) {
+        case '1':
+            playerThreeCalc.style.display = 'none';
+            playerFourCalc.style.display = 'none';
+            teamOneCalc.style.display = 'none';
+            teamTwoCalc.style.display = 'none';
+            break;
+        case '2':
+            playerFourCalc.style.display = 'none';
+            teamOneCalc.style.display = 'none';
+            teamTwoCalc.style.display = 'none';
+            break;
+        case '3':
+            teamOneCalc.style.display = 'none';
+            teamTwoCalc.style.display = 'none';
+            break;
+        case '4':
+            playerOneCalc.style.display = 'none';
+            playerTwoCalc.style.display = 'none';
+            playerThreeCalc.style.display = 'none';
+            playerFourCalc.style.display = 'none';
+            break;
+    }
+});
